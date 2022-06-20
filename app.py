98298d9edeb9da7c69225e1b31aced61ac7104d2
Patrick Sontag <pmsontag84@gmail.com>
2022-06-20 11:27:46 -0500

    add begin route

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+current_survey = satisfaction_survey
 
 user_responses = []
 
@@ -14,12 +17,14 @@ def home():
 
     return render_template("home.html")
 
-@app.route("/question")
-def question():
-    """Generate and show form to ask questions."""
-    ask = satisfaction_survey.questions[0].question
-    return render_template("question.html", ask=ask)
+@app.route("/begin", methods=["POST"])
+def begin():
+    """Redirect to first question"""
+    return redirect ("question.html", question_num=0)
 
+@app.route("/question/<int:qid>")
+def show_question(qid):
+    """Display current question"""
+    question = current_survey.questions[qid]
 
-# satisfaction_survey.questions[0].question
-# 'Have you shopped here before?'
+    return render_template("question.html", question_num=qid, question=question)
